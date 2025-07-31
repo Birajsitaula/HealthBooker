d964efa95a9a5238c45942420392f6feebb0c9fb
Birajsitaula <birajsitaula7@gmail.com>
2025-07-31 20:17:13 +0545

    Working on about us

--- client/src/components/AboutUs.jsx
+++ client/src/components/AboutUs.jsx
@@ -8,20 +8,17 @@ const AboutUs = () => {
         <h2 className="page-heading about-heading">About Us</h2>
         <div className="about">
           <div className="hero-img">
-            <img
-              src={image}
-              alt="hero"
-            />
+            <img src={image} alt="hero" />
           </div>
           <div className="hero-content">
             <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Quibusdam tenetur doloremque molestias repellat minus asperiores
-              in aperiam dolor, quaerat praesentium. Lorem ipsum dolor sit amet
-              consectetur adipisicing elit. Voluptatibus, repudiandae! Lorem
-              ipsum dolor sit amet consectetur adipisicing elit. Provident
-              quibusdam doloremque ex? Officia atque ab dolore? Tempore totam
-              non ea!
+              At HealthBooker, we are committed to revolutionizing healthcare
+              management with a user-friendly platform designed to streamline
+              appointments, enhance patient care, and support healthcare
+              professionals. Built with advanced technology, our system ensures
+              secure, efficient, and accessible services for patients, doctors,
+              and administrators alike. Our mission is to improve healthcare
+              delivery through innovation and collaboration.
             </p>
           </div>
         </div>
